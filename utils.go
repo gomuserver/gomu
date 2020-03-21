d33c4e888056268e374d203b66cdb5143cec9f2d
Lytic <d.halman@gmail.com>
2020-03-20 22:38:43 -0700

    gomu: Deploy local changes before incrementing version from v0.4.115

--- utils.go
+++ utils.go
@@ -2,72 +2,16 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"os"
-	"os/exec"
-	"path"
 	"strings"
 
+	com "github.com/hatchify/mod-common"
 	common "github.com/hatchify/mod-common"
-	sorter "github.com/hatchify/mod-sort"
+	gomu "github.com/hatchify/mod-utils"
 )
 
 var version = "undefined"
-var nameOnly bool
-
-// Println will print line if nameOnly isn't set
-func Println(a ...interface{}) (n int, err error) {
-	if !nameOnly {
-		n, err = fmt.Println(a...)
-	}
-
-	return
-}
-
-// Parses arguments to load target directories
-// Returns current lib if no args provided
-func getTargetDirs() (targetLibs sorter.StringArray) {
-	targetLibs = flag.Args()
-	if len(targetLibs) == 0 {
-		targetLibs = append(targetLibs, ".")
-	}
-	return
-}
-
-// Aggregates all libs within all target dirs
-func getLibsInAny(targetDirs []string) (libs sorter.StringArray) {
-	libs = make(sorter.StringArray, 0)
-	for index := range targetDirs {
-		libs = append(libs, getLibsInDirectory(targetDirs[index])...)
-	}
-
-	return
-}
-
-// Gets all libs in a given directory
-func getLibsInDirectory(dir string) (libs sorter.StringArray) {
-	cmd := exec.Command("ls")
-	cmd.Dir = dir
-	stdout, err := cmd.Output()
-
-	if err != nil {
-		return
-	}
-
-	// Parse files from exec "ls"
-	libs = strings.Split(string(stdout), "\n")
-	for index := range libs {
-		switch libs[index] {
-		case ".", "..", dir:
-			// Ignore non-repositories
-		default:
-			libs[index] = path.Join(dir, libs[index])
-		}
-	}
-
-	return
-}
 
 func readInput() {
 	var (
@@ -100,90 +44,102 @@ func showHelp() {
 	fmt.Println("")
 }
 
-func exit(status int) {
-	showHelp()
-	os.Exit(status)
-}
+func parseArgs() (options gomu.Options) {
+	options.LogLevel = com.NORMAL
 
-func showWarningOrQuit(message string) {
-	if !showWarning(message) {
-		Println("Exiting...")
-		exit(0)
-	}
-}
+	fmt.Println("Testing args...")
+	var argV = os.Args
+	var argC = len(argV)
+	fmt.Println(argC, "args:", argV)
 
-func showWarning(message string) (ok bool) {
-	if nameOnly {
-		// Don't show warnings for name only
-		return true
-	}
+	curFlag := ""
+	var arg *string
+	var gotTrailing = true
+	for i := 1; i < argC; i++ {
+		arg = &argV[i]
 
-	var err error
-	var text string
-	reader := bufio.NewReader(os.Stdin)
+		if strings.HasPrefix(*arg, "-") {
+			if !gotTrailing {
+				showHelp()
+				com.Errorln("Error: Unable to parse action.")
+				os.Exit(1)
+			}
 
-	for err == nil {
-		if text = strings.TrimSpace(text); len(text) > 0 {
-			switch text {
-			case "y", "Y", "Yes", "yes", "YES", "ok", "OK", "Ok":
-				ok = true
-				return
+			// Parse flags
+			curFlag = ""
+			switch *arg {
+			case "-name-only":
+				// Set value if boolean flag
+				options.LogLevel = com.NAMEONLY
 			default:
-				Println("Nevermind then! :)")
-				return
+				// Set flag if expecting trailing vailues
+				curFlag = *arg
+				gotTrailing = false
+			}
+		} else {
+			if i == argC-1 {
+				// End of args
+				if gotTrailing {
+					// Satisfied previous arg
+					if len(options.Action) == 0 {
+						// We need an action.. this one should do?
+						options.Action = *arg
+						break
+					}
+				}
+			}
+			gotTrailing = true
+
+			// Parse args
+			switch curFlag {
+			case "-branch", "-b":
+				options.Branch = *arg
+				curFlag = ""
+			case "-dep", "-depends", "-filter", "-f":
+				options.FilterDependencies = append(options.FilterDependencies, *arg)
+			case "-dir", "-directory", "-target", "-t":
+				options.TargetDirectories = append(options.TargetDirectories, *arg)
+			case "-log", "-level", "-log-level", "-l":
+				if options.LogLevel != com.NAMEONLY {
+					// Ignore log level if name-only is set
+					options.LogLevel = com.LogLevelFrom(*arg)
+				}
+				curFlag = ""
+			case "":
+				if len(options.Action) == 0 {
+					// Comand
+					options.Action = *arg
+				} else {
+					// Arg
+					options.Args = append(options.Args, *arg)
+				}
 			}
 		}
-
-		// No newline. name-only already exited above
-		fmt.Print(message + " [y|yes|ok]: ")
-		text, err = reader.ReadString('\n')
 	}
 
-	Println("Oops... Something went wrong.")
+	if len(options.TargetDirectories) == 0 {
+		options.TargetDirectories = []string{"."}
+	}
+	options.Action = strings.ToLower(options.Action)
+
+	if len(options.Action) == 0 {
+		// Error parsing
+		showHelp()
+		com.Errorln("Error: Unable to parse action.")
+		os.Exit(1)
+	}
+
 	return
 }
 
-func checkArgs(action, branch, tag *string, filterDeps, targetDirs *sorter.StringArray, debug, verbose, nameOnly *bool) {
-	// Get optional args for forcing a tag number, setting branches, and passing actions
-	flag.StringVar(action, "action", "", "function to perform [list|sync|deploy|pull]")
-	flag.StringVar(branch, "branch", "master", "branch to user when pull (and eventually pull request) are used. Default to master (eventually default to current)")
-	flag.StringVar(tag, "tag", "", "optional value to set for git tag")
+func checkArgs() *gomu.MU {
+	options := parseArgs()
+	common.SetLogLevel(options.LogLevel)
 
-	// Filter/Aggregator
-	flag.Var(filterDeps, "dep", "optional dependency filter: accepts multiple -dep flags to only list/sort libs which depend on one of the provided filters")
-	flag.Var(targetDirs, "dir", "optional directory aggregator: accepts multiple -dir flags to aggregate libs in multiple organizations")
-
-	// Output flags
-	flag.BoolVar(debug, "debug", false, "optional value to get debug output")
-	flag.BoolVar(verbose, "verbose", true, "optional value to print progress output")
-	flag.BoolVar(nameOnly, "name-only", false, "optional value to minimize output to just the sorted paths of libs that were changed by gomu. Note, this overrides debug and verbose flags")
-
-	// Load flags
-	flag.Parse()
-
-	// Set output level (TODO: Log level?)
-	common.SetDebug(*debug && !*nameOnly)
-	common.SetVerbose(*verbose && !*nameOnly)
-
-	// Set default return
-	command := *action
-
-	// Check for conflict in action vs args to parse command
-	if len(flag.Args()) == 1 {
-		if len(command) != 0 {
-			if command != flag.Arg(0) {
-				// Conflict?
-				fmt.Println("\nError: Unable to parse action: <" + command + "> from command: <" + flag.Arg(0) + ">")
-				exit(1)
-			}
-		} else {
-			command = flag.Arg(0)
-		}
-	}
+	// TODO: Validate args/flags?
 
 	// Check for supported actions
-	command = strings.ToLower(command)
-	switch command {
+	switch options.Action {
 	case "list", "pull", "reset", "replace-local":
 		// Public commands
 
@@ -196,40 +152,14 @@ func checkArgs(action, branch, tag *string, filterDeps, targetDirs *sorter.Strin
 		os.Exit(0)
 	case "help":
 		// Print help and exit without error
-		exit(0)
+		showHelp()
+		os.Exit(0)
 	default:
 		// Show usage and exit with error
-		fmt.Println("\nError: Unsupported action: <" + command + ">")
-		exit(1)
+		com.Errorln("\nError: Unsupported action: <" + options.Action + ">")
+		showHelp()
+		os.Exit(1)
 	}
 
-	// Set defaults if necessary
-	if len(*targetDirs) == 0 {
-		*targetDirs = append(*targetDirs, ".")
-	}
-
-	if len(*filterDeps) == 0 {
-		*filterDeps = append(*filterDeps, "")
-	}
-
-	*action = command
-	return
-}
-
-func performPull(branch string, itr *sorter.FileNode) (success bool) {
-	success = true
-
-	if itr.File.CheckoutBranch(branch) != nil {
-		itr.File.Output("Failed to checkout " + branch + " :(")
-		success = false
-	}
-
-	if itr.File.Pull() == nil {
-		itr.File.Output("Pull successful!")
-	} else {
-		itr.File.Output("Failed to pull " + branch + " :(")
-		success = false
-	}
-
-	return
+	return gomu.New(options)
 }
